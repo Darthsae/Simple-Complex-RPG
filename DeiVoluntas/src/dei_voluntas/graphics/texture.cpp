50ac2fb9a67d0b9297ff1bb3d7a7ecd3b6a8a8a0
Markus L. Tanner <70599537+Darthsae@users.noreply.github.com>
2024-10-24 09:19:55 -0400

    Update texture.cpp

--- DeiVoluntas/src/dei_voluntas/graphics/texture.cpp
+++ DeiVoluntas/src/dei_voluntas/graphics/texture.cpp
@@ -4,4 +4,12 @@
 using namespace DeiVoluntas::Graphics;
 
 Texture::Texture(SDL_Renderer* renderer, std::string path) {
+    SDL_Surface *surface = IMG_Load(path.c_str());
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    size = Vec2i(surface->w, surface->h);
+    SDL_FreeSurface(surface);
+}
+
+Texture::~Texture() {
+    delete texture;
 }
