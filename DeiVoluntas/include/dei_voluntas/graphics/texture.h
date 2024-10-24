bba4d0ceae6bce3d9eebb61309f3ab196477c5d7
Markus L. Tanner <70599537+Darthsae@users.noreply.github.com>
2024-10-24 09:12:25 -0400

    Update texture.h

--- DeiVoluntas/include/dei_voluntas/graphics/texture.h
+++ DeiVoluntas/include/dei_voluntas/graphics/texture.h
@@ -1,5 +1,17 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 #include <SDL.h>
+#include "dei_voluntas/data/vec.h"
 
+using DeiVoluntas::Data::Vec2i;
+
+namespace DeiVoluntas::Graphics {
+    struct Texture {
+        Texture(SDL_Renderer* renderer, std::string path);
+        ~Texture();
+
+        SDL_Texture* texture;
+        Vec2i size;
+    };
+}
 #endif
